3e2ef6b34dbdee08aa9467e682b05e1af7feffa8
Lucas Elizalde <lcselizalde@gmail.com>
2025-04-05 02:54:37 -0300

    Sitio web finalizado

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,11 @@
-// Este script alterna la visibilidad de los enlaces de navegación en el navbar cuando se hace clic en el botón de menú (hamburguesa).
 document.addEventListener("DOMContentLoaded", () => {
-    const toggle = document.getElementById("menu-toggle");
-    const links = document.querySelector(".navbar__links");
-  
+  const toggle = document.getElementById("menu-toggle");
+  const links = document.querySelector(".navbar__links");
+
+  // Solo si existen los elementos
+  if (toggle && links) {
     toggle.addEventListener("click", () => {
       links.classList.toggle("active");
     });
-  });
-  
+  }
+});

aa64f20eaa6f47549ddd28b51028218059644621
Lucas Elizalde <lcselizalde@gmail.com>
2025-04-04 23:21:55 -0300

    Footer y Header responsives

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,4 @@
-// Este script alterna la visibilidad de los enlaces de navegación en el navbar
+// Este script alterna la visibilidad de los enlaces de navegación en el navbar cuando se hace clic en el botón de menú (hamburguesa).
 document.addEventListener("DOMContentLoaded", () => {
     const toggle = document.getElementById("menu-toggle");
     const links = document.querySelector(".navbar__links");
